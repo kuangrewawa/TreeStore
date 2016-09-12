fd19068642a1b370246ced911bd6a95826c7ecc2
xuzhang <xuzhang.xuzhang@huawei.com>
2016-09-12 16:39:32 +0800

    YangTree

---
+++ YangTreeStore.java
@@ -0,0 +1,23 @@
+/*
+ *Copyright (c) 2014 Cisco Systems, Inc. and others. All rights reserved. 
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/www.eclipse.org/legal/epl-v10.html
+ */
+package org.onosproject.yangtree;
+
+
+public interface YangTreeStore {
+	
+	CheckedFuture<Optional<YangTreeNode>,ReadFailedException> get(LogicalTreeType store, YangTreePath path, Filter filter)
+	
+	CheckedFuture<FailedException> write(LogicalTreeType store, YangTreePath path, YangTreeNode data);
+	
+	CheckedFuture<FailedException> update(LogicalTreeType store, YangTreePath path, YangTreeNode data);
+	
+	CheckedFuture<FailedException> delete(LogicalTreeType store, YangTreePath path);
+	
+	<L extends YangTreeNodeChangeListener> ListenerRegistration<L> registerTreeNodeChangeListener(YangTreePath treeId, L listener)
+
+}
