b519670c232e09a4eff6a72e0ea2048684a521e0
jiangcc <jiangcc861003@gmail.com>
2016-09-13 19:34:39 +0800

    Update YangTreeStore.java

--- YangTreeStore.java
+++ YangTreeStore.java
@@ -10,14 +10,47 @@ package org.onosproject.yangtree;
 
 public interface YangTreeStore {
 	
+	/**
+	 * 
+	 * @param store  controller can support more than one tree, ONOS can default define a NE-Config tree and app also define its tree. 
+	 * LogicalTreeType is the identifier  of tree.
+	 * @param path  each YangTreePath carry ancestor node path. It is a recursion data structure.
+	 * @param filter most of scene apps want to read  a given YangTreeNode or subtree node. But we hope Filter provide a extension mechanism , user can define filter criteria on demand.
+	 * @return YangTreeNode a node or a subtree
+	 * /
 	CheckedFuture<Optional<YangTreeNode>,ReadFailedException> get(LogicalTreeType store, YangTreePath path, Filter filter);
-	
+	/**
+	 * Eg: a/b/c/d was the path given; if node b is not present, assume c &d cannot be present; go on to create node b, c as its only child & node d as c’s only child
+	 * 
+	 * @param store  controller can support more than one tree, ONOS can default define a NE-Config tree and app also define its tree. 
+	 * LogicalTreeType is the identifier  of tree.
+	 * @param path  each YangTreePath carry ancestor node path. It is a recursion data structure.
+	 * @param data  all parent nodes of this node should be present.
+	 * /
 	CheckedFuture<FailedException> write(LogicalTreeType store, YangTreePath path, YangTreeNode data);
-	
+	/**
+	 * 
+	 * @param store  controller can support more than one tree, ONOS can default define a NE-Config tree and app also define its tree. 
+	 * LogicalTreeType is the identifier  of tree.
+	 * @param path  specific path must be present.
+	 * @param data  the new value.
+	 * /
 	CheckedFuture<FailedException> update(LogicalTreeType store, YangTreePath path, YangTreeNode data);
-	
+	/**
+	 * 
+	 * @param store  controller can support more than one tree, ONOS can default define a NE-Config tree and app also define its tree. 
+	 * LogicalTreeType is the identifier  of tree.
+	 * @param path  specific path must be present.
+	 * /
 	CheckedFuture<FailedException> delete(LogicalTreeType store, YangTreePath path);
-	
-	<L extends YangTreeNodeChangeListener> ListenerRegistration<L> registerTreeNodeChangeListener(YangTreePath treeId, L listener);
+	/**
+	 * 
+	 * @param store  controller can support more than one tree, ONOS can default define a NE-Config tree and app also define its tree. 
+	 * LogicalTreeType is the identifier  of tree.
+	 * @param path  specific path must be present.
+	 * @param mode  enum type, listen a node or a subtree.
+	 * @param listener
+	 * /
+	<L extends YangTreeNodeChangeListener> ListenerRegistration<L> registerTreeNodeChangeListener(LogicalTreeType store, YangTreePath treeId, Mode mode, L listener);
 
 }
